5cd92f1d056a246afa9432153055e83d54f0309b
LauraDev <lauralodeirodev@gmail.com>
2024-10-01 08:53:44 +0200

    Añadimos funcionalidad al pinchar en un pokemon

--- Desktop/Pokemon/app/[id].tsx
+++ Desktop/Pokemon/app/[id].tsx
@@ -1,19 +1,58 @@
+import { PokemonDetails } from "@/types/app";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const PokemonDetailsProps=()=>{
     const{id}=useLocalSearchParams();
+    const[pokemonDetails,setPokemonDetails]=useState<PokemonDetails>();
+    const [isLoading,setIsLoading]=useState(false);
+
+    useEffect(()=>{
+        setIsLoading(true);
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then(response=>response.json())
+        .then(data=>setPokemonDetails(data))
+        .finally(()=>setIsLoading(false));
+
+    },[id]);
+
+    if(isLoading){
+        return(
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.loading}>Loading...</Text>
+            </SafeAreaView>
+        );
+    };
     return(
-        <SafeAreaView>
-        <Text style={styles.text}>{id}</Text>
+        <SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.card}>
+
+        </ScrollView>
         </SafeAreaView>
     )
 };
 
 const styles = StyleSheet.create({
-    text: {
-        textAlign: 'center',
+    container:{
+        flex:1,
+        justifyContent: "center",
+        alignItems:"center",
     },
+    loading:{
+        textAlign:"center",
+        fontSize:24,
+    },
+    card:{
+        flex:1,
+        justifyContent: "center",
+        alignItems:"center",
+        backgroundColor:"pink",
+        height:500,
+        width:300,
+    },
+    
 })
 
 export default PokemonDetailsProps;
--- Desktop/Pokemon/types/app.d.ts
+++ Desktop/Pokemon/types/app.d.ts
@@ -2,3 +2,30 @@ export type Pokemon={
     name:string;
     url:string;
 }
+type PokemonType={
+    slot:number;
+    type:{
+        name:string;
+        url:string;
+    };
+}
+
+export type PokemonDetails={
+    cries:{
+        latest: string;
+    };
+    heigh:number;
+    id:number;
+    name:string;
+    sprites:{
+        other:{
+            "official-artwork":{
+                front_default:string;
+            };
+        };
+    };
+    typer:PokemonType[];
+    weight: number;
+
+
+}
