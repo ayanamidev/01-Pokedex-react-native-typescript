017f7bf0e2ef291828aec5ab10a692500a8bf8ae
LauraDev <lauralodeirodev@gmail.com>
2025-02-11 16:12:35 +0100

    No se puede usar avatar

--- Desktop/ecommerce-react-native/app/(tabs)/cuenta/userLogged.tsx
+++ Desktop/ecommerce-react-native/app/(tabs)/cuenta/userLogged.tsx
@@ -37,62 +37,20 @@ const UserLogged = () => {
 
   
 
-  const changeAvatar = async () => {
-    // Pedir permisos
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== 'granted') {
-      Alert.alert("Permiso denegado", "Necesitas permitir el acceso a la galería.");
-      return;
-    }
-  
-    console.log("Permiso concedido, abriendo galería...");
-  
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: 'images',
-        allowsEditing: true,
-        aspect: [1, 1],
-        quality: 1,
-      });
-  
-      if (!result.canceled) {
-        console.log("Imagen seleccionada:", result.assets[0].uri);
-      } else {
-        console.log("Selección cancelada.");
-      }
-    } catch (error) {
-      console.error("Error al abrir la galería:", error);
-  
-      // En Android, intenta abrir la galería manualmente
-      if (Platform.OS === "android") {
-        IntentLauncher.startActivityAsync(IntentLauncher.ActivityAction.PICK);
-      }
-    }
-  };
-
   return (
     <View style={styles.content}>
-      <Avatar
-        size="large"
-        rounded
-        icon={{ type: "material", name: "person" }}
-        containerStyle={styles.avatar}
-      >
-        <Avatar.Accessory size={24} onPress={changeAvatar} />
-
-      </Avatar>
-      <View>
+  
+    
        <Button
         mode="contained"
         onPress={logout}
-        
+        buttonColor='#ff0000'
         icon={() => <FontAwesome name="sign-out" size={24} color="white" />}
       >
         Cerrar sesión
       </Button> 
       </View>
-      
-    </View>
+    
   );
 };
 
@@ -100,14 +58,13 @@ const styles = StyleSheet.create({
   content: {
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'row',
+    flex: 1,
     backgroundColor: '#f2f2f2',
     paddingVertical: 30,
 
   },
-  avatar:{
-    marginRight: 20,
-    backgroundColor:"green"
+  button: {
+    
   }
 });
 
